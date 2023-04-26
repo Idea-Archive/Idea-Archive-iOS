b8340016f14f7088628b4b37e0107d8bb3071563
junseopark0331 <junseopark0331@icloud.com>
2023-04-26 18:03:26 +0900

    프로젝트 초기 세팅

---
+++ Idea-Archive/Sources/Utility/Base/BaseViewController.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+class BaseViewController: UIViewController {
+    // MARK: - Properties
+    let bounds = UIScreen().bounds
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupBackgroundIfNotSet()
+        setup()
+        addView()
+        setup()
+        setLayout()
+        bind()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        setLayoutSubviews()
+    }
+    
+    // MARK: - Deinitalizer
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        print("\(type(of: self)): \(#function)")
+    }
+    // MARK: - Method
+    private func setupBackgroundIfNotSet() {
+        if self.view.backgroundColor == nil {
+            self.view.backgroundColor = .white
+        }
+    }
+    
+    func setup() {}
+    func addView() {}
+    func setLayout() {}
+    func setLayoutSubviews() {}
+    
+    func bind() {}
+}
+
+
