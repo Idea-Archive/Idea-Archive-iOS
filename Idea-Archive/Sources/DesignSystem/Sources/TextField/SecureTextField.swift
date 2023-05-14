f7171147a7a270c6a844a2aa1a6437a20788129f
junseopark0331 <junseopark0331@icloud.com>
2023-05-14 23:42:25 +0900

    Font 이름별로 설정, placeholder의 색깔을 원하는 색깔로 지정할 수 있게끔 했습니다

--- Idea-Archive/Sources/DesignSystem/Sources/TextField/SecureTextField.swift
+++ Idea-Archive/Sources/DesignSystem/Sources/TextField/SecureTextField.swift
@@ -1,45 +1,47 @@
 import UIKit
 
-public final class SecureTextField: UITextField{
+public final class SecureTextField: UITextField {
     
-    var passwordEyeButton = UIButton(type: .custom)
+    var passwordToggleButton = UIButton(type: .custom)
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        self.placeholder = placeholder
         setupView()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        setupView()
     }
     
-    func setupView(){
+    func setupView() {
         borderStyle = .roundedRect
         layer.cornerRadius = 10
         backgroundColor = UIColor(rgb: 0xF1F1F5)
+        font = .I_A(size: 14, family: .regular)
+        setPlaceholderColor(UIColor(rgb: 0x99999))
         
         isSecureTextEntry = true
-        setPasswordShownButtonImage()
+        setPasswordToggleButtonImage()
     }
     
-    func setPasswordShownButtonImage () {
-        passwordEyeButton = UIButton.init (primaryAction: UIAction (handler: { [self]_ in
+    func setPasswordToggleButtonImage() {
+        passwordToggleButton = UIButton.init (primaryAction: UIAction (handler: { [self]_ in
             isSecureTextEntry.toggle()
-            self.passwordEyeButton.isSelected.toggle ()
+            self.passwordToggleButton.isSelected.toggle ()
         }))
         
         var buttonConfiguration = UIButton.Configuration.plain()
         buttonConfiguration.imagePadding = 10
         buttonConfiguration.baseBackgroundColor = .clear
         
-        passwordEyeButton.setImage (UIImage (systemName: "eye")?.withTintColor(UIColor(rgb: 0x999999), renderingMode: .alwaysOriginal), for: .normal)
-        passwordEyeButton.setImage(UIImage(systemName: "eye.slash")?.withTintColor(UIColor(rgb: 0x999999), renderingMode: .alwaysOriginal), for: .selected)
+        passwordToggleButton.setImage (UIImage (systemName: "eye")?.withTintColor(UIColor(rgb: 0x999999), renderingMode: .alwaysOriginal), for: .normal)
+        passwordToggleButton.setImage(UIImage(systemName: "eye.slash")?.withTintColor(UIColor(rgb: 0x999999), renderingMode: .alwaysOriginal), for: .selected)
         
-        passwordEyeButton.configuration = buttonConfiguration
-        passwordEyeButton.isSelected.toggle()
+        passwordToggleButton.configuration = buttonConfiguration
+        passwordToggleButton.isSelected.toggle()
         
-        rightView = passwordEyeButton
+        rightView = passwordToggleButton
         rightViewMode = .always
     }
 }
