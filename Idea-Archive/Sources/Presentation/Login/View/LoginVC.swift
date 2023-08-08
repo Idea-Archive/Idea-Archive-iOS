085829567103ee03ec48d261e7056577c6317416
junseopark0331 <junseopark0331@icloud.com>
2023-08-08 12:22:25 +0900

    :lipstick: 로그인 페이지를 수정된 디자인에 맞게 퍼블리싱 했습니다.
    로그인 페이지를 수정된 디자인에 맞게 퍼블리싱

--- Idea-Archive/Sources/Presentation/Login/View/LoginVC.swift
+++ Idea-Archive/Sources/Presentation/Login/View/LoginVC.swift
@@ -16,22 +16,27 @@ final class LoginVC: BaseVC {
         $0.font = .I_A(size: 14, family: .regular)
     }
     
+    private lazy var gotoSignupButton = AdditionalButton().then{
+        $0.setTitle("회원가입", for: .normal)
+        $0.setTitleColor(UIColor(rgb: 0x999999), for: .normal)
+        $0.addTarget(self, action: #selector(signupButtonTapped), for: .touchUpInside)
+    }
+    
+    private let betweenView = UIView().then{
+        $0.backgroundColor = UIColor(rgb: 0x999999)
+    }
+    
+    private lazy var findPasswordButton = AdditionalButton().then{
+        $0.setTitle("비밀번호 찾기", for: .normal)
+        $0.setTitleColor(UIColor(rgb: 0x999999), for: .normal)
+        $0.addTarget(self, action: #selector(findPasswordButtonTapped), for: .touchUpInside)
+    }
+    
     private lazy var loginButton = NextStepButton().then{
         $0.setTitle("로그인", for: .normal)
         $0.isEnabled = false
         $0.backgroundColor = UIColor(rgb: 0x999999)
         $0.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
-    }
-    
-    private let forgotPaswordLabel = SmallTitleLabel().then{
-        $0.text = "비밀번호를 잊으셨나요?"
-        $0.textColor = UIColor(rgb: 0x191919)
-    }
-    
-    private lazy var findPasswordButton = AdditionalButton().then{
-        $0.setTitle("비밀번호 찾기", for: .normal)
-        $0.setTitleColor(UIColor(rgb: 0x35B2CD), for: .normal)
-        $0.addTarget(self, action: #selector(findPasswordButtonTapped), for: .touchUpInside)
     }
     
     private lazy var googleLoginButton = UIButton().then{
@@ -58,17 +63,6 @@ final class LoginVC: BaseVC {
         $0.clipsToBounds = true
     }
     
-    private let firstTimeIdeaArchiveLabel = SmallTitleLabel().then{
-        $0.text = "IA가 처음이신가요?"
-        $0.textColor = UIColor(rgb: 0x767676)
-    }
-    
-    private lazy var gotoSignupButton = AdditionalButton().then{
-        $0.setTitle("회원가입", for: .normal)
-        $0.setTitleColor(UIColor(rgb: 0x191919), for: .normal)
-        $0.addTarget(self, action: #selector(signupButtonTapped), for: .touchUpInside)
-    }
-    
     override func setup() {
         self.navigationController?.navigationBar.tintColor = UIColor(rgb: 0x000000)
         self.navigationController?.navigationBar.topItem?.title = ""
@@ -76,85 +70,92 @@ final class LoginVC: BaseVC {
         passwordTextField.delegate = self
     }
     
-    
     override func addView() {
         view.addSubview(loginLabel)
         view.addSubview(idTextField)
         view.addSubview(passwordTextField)
-        view.addSubview(loginButton)
-        view.addSubview(forgotPaswordLabel)
+        view.addSubview(gotoSignupButton)
+        view.addSubview(betweenView)
         view.addSubview(findPasswordButton)
-        view.addSubview(firstTimeIdeaArchiveLabel)
+        view.addSubview(loginButton)
+        view.addSubview(findPasswordButton)
         view.addSubview(gotoSignupButton)
         view.addSubview(appleLoginButton)
         view.addSubview(googleLoginButton)
         view.addSubview(kakaoLoginButton)
         view.addSubview(githubLoginButton)
-        
     }
     
     override func setLayout(){
+        
         self.loginLabel.snp.makeConstraints{
             $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(52)
             $0.centerX.equalTo(view.snp.centerX)
         }
+        
         self.idTextField.snp.makeConstraints{
             $0.height.equalTo(52)
             $0.top.equalTo(loginLabel.snp.bottom).offset(64)
             $0.leading.trailing.equalTo(self.view).inset(28)
         }
+        
         self.passwordTextField.snp.makeConstraints{
             $0.height.equalTo(52)
             $0.top.equalTo(idTextField.snp.bottom).offset(12)
             $0.leading.trailing.equalTo(self.view).inset(28)
         }
+        
+        self.gotoSignupButton.snp.makeConstraints{
+            $0.bottom.equalTo(findPasswordButton.snp.bottom)
+            $0.trailing.equalTo(self.betweenView.snp.leading).inset(-8)
+        }
+
+        self.betweenView.snp.makeConstraints{
+            $0.height.equalTo(16)
+            $0.width.equalTo(1)
+            $0.bottom.equalTo(self.loginButton.snp.top).offset(-16)
+            $0.trailing.equalTo(self.findPasswordButton.snp.leading).offset(-8)
+        }
+        
+        self.findPasswordButton.snp.makeConstraints{
+            $0.bottom.equalTo(self.loginButton.snp.top).offset(-12)
+            $0.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(28)
+        }
+        
         self.loginButton.snp.makeConstraints{
             $0.height.equalTo(52)
             $0.top.equalTo(passwordTextField.snp.bottom).offset(48)
-            $0.leading.equalTo(self.view).offset(24)
+            $0.leading.equalTo(
+                self.view).offset(24)
             $0.centerX.equalTo(view.snp.centerX)
         }
-        self.forgotPaswordLabel.snp.makeConstraints{
-            $0.top.equalTo(loginButton.snp.bottom).offset(12)
-            $0.leading.equalTo(self.view).offset(94)
-        }
-        self.findPasswordButton.snp.makeConstraints{
-            $0.height.equalTo(20)
-            $0.top.equalTo(loginButton.snp.bottom).offset(10)
-            $0.leading.equalTo(forgotPaswordLabel.snp.trailing).offset(4)
-        }
+        
         self.appleLoginButton.snp.makeConstraints{
             $0.height.width.equalTo(36)
-            $0.bottom.equalTo(firstTimeIdeaArchiveLabel.snp.bottom).inset(32)
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).inset(96)
             $0.trailing.equalTo(googleLoginButton.snp.leading).offset(-28)
         }
+        
         self.googleLoginButton.snp.makeConstraints{
             $0.height.width.equalTo(36)
-            $0.bottom.equalTo(firstTimeIdeaArchiveLabel.snp.bottom).inset(32)
+            $0.bottom.equalTo(self.appleLoginButton.snp.bottom)
             $0.centerX.equalToSuperview().offset(-32)
         }
+        
         self.kakaoLoginButton.snp.makeConstraints{
             $0.height.width.equalTo(36)
-            $0.bottom.equalTo(firstTimeIdeaArchiveLabel.snp.bottom).inset(32)
+            $0.bottom.equalTo(self.appleLoginButton.snp.bottom)
             $0.leading.equalTo(view.snp.centerX).offset(14)
         }
+        
         self.githubLoginButton.snp.makeConstraints{
             $0.height.width.equalTo(36)
-            $0.bottom.equalTo(firstTimeIdeaArchiveLabel.snp.bottom).inset(32)
+            $0.bottom.equalTo(self.appleLoginButton.snp.bottom)
             $0.leading.equalTo(kakaoLoginButton.snp.trailing).offset(28)
-        }
-        self.firstTimeIdeaArchiveLabel.snp.makeConstraints{
-            $0.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(12)
-            $0.leading.equalTo(self.view).offset(106)
-        }
-        self.gotoSignupButton.snp.makeConstraints{
-            $0.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(5)
-            $0.leading.equalTo(firstTimeIdeaArchiveLabel.snp.trailing).offset(4)
         }
     }
     
     @objc func loginButtonTapped(_ sender: UIButton){
-        print("로그인 버튼 클릭")
         LoginViewModel().loginCompleted(email: idTextField.text ?? "", password: passwordTextField.text ?? "")
     }
     
@@ -167,7 +168,6 @@ final class LoginVC: BaseVC {
         let vc = SignupViewController()
         self.navigationController?.pushViewController(vc, animated: true)
     }
-    
 }
 
 extension LoginVC: UITextFieldDelegate{
