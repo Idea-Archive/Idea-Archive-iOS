549fe0c8bedd7c20a0ed079966f8779ce7cc6d73
uuuunseo <jdbstjoo@gmail.com>
2023-08-27 23:27:57 +0900

    :recycle: :: [43] tuist / project.swift conflict 부분 설정

--- Project.swift
+++ Project.swift
@@ -1,34 +1,5 @@
 import ProjectDescription
 
-<<<<<<< Updated upstream
-// MARK: Constants
-let projectName = "Idea-Archive"
-let organizationName = "Idea-Archive-iOS"
-let bundleID = "class.Idea-Archive"
-let targetVersion = "3.19.0"
-
-// MARK: Struct
-let project = Project(
-    name: projectName,
-    organizationName: organizationName,
-    settings: nil,
-    packages: [],
-    targets: [
-        Target(name: projectName,
-               platform: .iOS,
-               product: .app,
-               bundleId: bundleID,
-               deploymentTarget: .iOS(targetVersion: targetVersion, devices: [.iphone, .ipad]),
-               infoPlist: .default,
-               sources: ["\(projectName)/**"],
-               resources: [],
-               dependencies: []
-              )
-    ],
-    schemes: [
-        Scheme(name: "\(projectName)-debug"),
-        Scheme(name: "\(projectName)-Release")
-=======
 let projectName = "Idea-Archive"
 let orginazationIden = ".class"
 
@@ -76,7 +47,6 @@ let project = Project(
                 .package(product: "Then")
             ]
         )
->>>>>>> Stashed changes
     ],
     fileHeaderTemplate: nil,
     additionalFiles: [],
