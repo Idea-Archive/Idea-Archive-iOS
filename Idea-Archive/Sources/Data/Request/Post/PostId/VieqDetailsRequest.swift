ea3aebde51156f6d19299b80e77b08c72f3ede26
uuuunseo <jdbstjoo@gmail.com>
2023-09-30 09:11:16 +0900

    :recycle: :: Post / WriteArticleRequest 수정

---
+++ Idea-Archive/Sources/Data/Request/Post/PostId/VieqDetailsRequest.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct ViewDetailsRequest: Codable {
+    let postId: String
+}
--- Idea-Archive/Sources/Data/Request/Post/Write/WriteArticleRequest.swift
+++ Idea-Archive/Sources/Data/Request/Post/Write/WriteArticleRequest.swift
@@ -5,4 +5,11 @@ struct WriteArticleRequest: Codable {
     let title: String
     let content: String
     let category: [String]
+    
+    init(Authorization: String, title: String, content: String, category: [String]) {
+        self.Authorization = Authorization
+        self.title = title
+        self.content = content
+        self.category = category
+    }
 }
